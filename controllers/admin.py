a79edfcbd7a7e5084c44a842e017f1b8a92e8e91
ChuYuan Chiang <chuyuan.chiang@gmail.com>
2021-09-15 16:49:15 +0800

    adjust api/pin behavior, run api without login, and add admin/user_group api

--- controllers/admin.py
+++ controllers/admin.py
@@ -17,3 +17,15 @@ def user_status_manage():
     showmsg = showmsg + 'is login: {} '.format(auth.is_logged_in())
     showmsg = showmsg + 'has permission: {} '.format(auth.has_permission('edit', db.t_user_status, 0, auth.user.id))
     return locals()
+
+auth.settings.actions_disabled.append('register')
+auth.settings.login_url = URL('admin', 'user/login')
+@auth.requires_login()
+@auth.requires_permission('edit', db.auth_membership)
+def user_group():
+    form = SQLFORM.smartgrid(db.auth_membership,onupdate=auth.archive)
+    #form = None
+    showmsg = 'group id: {} '.format(auth.id_group('admin'))
+    showmsg = showmsg + 'is login: {} '.format(auth.is_logged_in())
+    showmsg = showmsg + 'has permission: {} '.format(auth.has_permission('edit', db.auth_membership, 0, auth.user.id))
+    return locals()
--- models/db_wizard.py
+++ models/db_wizard.py
@@ -11,8 +11,14 @@ db.define_table('t_user_status',
           label=T('Expire Time')),
     Field('f_permission', type='integer',
           label=T('Permission')),
+    Field('f_uuid', type='string',
+          label=T('uuid')),
+    Field('f_email', type='string',
+          label=T('email')),
+    Field('f_comment', type='string',
+          label=T('comments')),
     auth.signature,
-    format='%(f_user_id)s',
+    format='anonymous',
     migrate=settings.migrate)
 
 db.define_table('t_user_status_archive',db.t_user_status,Field('current_record','reference t_user_status',readable=False,writable=False))
